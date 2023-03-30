e880a8addc965773f83886960e1d48c6c88fed11
Ulrich Mabou <ulrichmabou@gmail.com>
2023-03-30 20:01:18 +0200

    test agent by running a chain based on user input

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
 import * as dotenv from "dotenv";
-import { OpenAI } from "langchain/llms";
-import { PromptTemplate } from "langchain/prompts";
-import { LLMChain } from "langchain/chains";
+import { OpenAI } from "langchain";
+// import { PromptTemplate } from "langchain/prompts";
+// import { LLMChain } from "langchain/chains";
+import { initializeAgentExecutor } from "langchain/agents";
+import { SerpAPI, Calculator } from "langchain/tools";
+
 
 dotenv.config();
 
@@ -27,17 +30,37 @@ dotenv.config();
 
 // LLMChain: PromptTemplate + LLM
 
-const model = new OpenAI({ temperature: 0.9 });
-const template = "What is a good name for a company that makes {product}?";
-const prompt = new PromptTemplate({
-  template: template,
-  inputVariables: ["product"],
-});
+// const model = new OpenAI({ temperature: 0.9 });
+// const template = "What is a good name for a company that makes {product}?";
+// const prompt = new PromptTemplate({
+//   template: template,
+//   inputVariables: ["product"],
+// });
 
 
-const chain = new LLMChain({ llm: model, prompt: prompt });
+// const chain = new LLMChain({ llm: model, prompt: prompt });
 
-const res = await chain.call({ product: "colorful socks" });
+// const res = await chain.call({ product: "colorful socks" });
 
-console.log(res);
+// console.log(res);
 
+// Agents: Dynamically Run Chains Based on User Input
+
+const model = new OpenAI({ temperature: 0 });
+const tools = [new SerpAPI(), new Calculator()];
+
+const executor = await initializeAgentExecutor(
+  tools,
+  model,
+  "zero-shot-react-description"
+);
+console.log("Loaded agent.");
+
+const input =
+  "Who is Olivia Wilde's boyfriend?" +
+  " What is his current age raised to the 0.23 power?";
+console.log(`Executing with input "${input}"...`);
+
+const result = await executor.call({ input });
+
+console.log(`Got output ${result.output}`);
